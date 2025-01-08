3b0800297223955e7b97f9db0e1f41e8d5e8552f
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-08 15:25:46 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_slug>/', views.category_page, name='category_page'),
+    path('profile/<str:username>/', views.user_profile, name='user_profile'),
+    path('contact/', views.contact, name='contact'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
 ]
