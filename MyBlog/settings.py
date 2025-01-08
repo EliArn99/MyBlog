d9f2ea2d83c7ab4cf77dfb9a63f3f895284b77a7
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-08 15:19:10 +0200

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MyBlog.blog.apps.BlogConfig'
+    'MyBlog.blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
