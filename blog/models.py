82c9c2e4b5170bcd99a6cdccedb2e09fafd3ecf7
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-13 16:48:02 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,14 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
