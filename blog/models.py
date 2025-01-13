23d390481081109b1555e4e2d595e12d1c0914b5
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-13 16:48:45 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -46,3 +46,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
