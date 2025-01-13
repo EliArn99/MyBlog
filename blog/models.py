fce2c15a272c66eb3bb0dc056b7ebd901287e3b3
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-13 16:47:49 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
