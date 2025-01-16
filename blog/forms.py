a88da1e9ca0b31033d0bef85533542501864fb00
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-16 09:19:30 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,1 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    bio = forms.CharField(widget=forms.Textarea, required=False)
+    profile_picture = forms.ImageField(required=False)
+    location = forms.CharField(max_length=100, required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2', 'bio', 'profile_picture', 'location']
 
