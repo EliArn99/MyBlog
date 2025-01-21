43595e0ff49f9b2344333ee5334e5aa4424667d3
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-21 13:04:24 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
-
+from .models import Book
 class CustomUserCreationForm(UserCreationForm):
     bio = forms.CharField(widget=forms.Textarea, required=False)
     profile_picture = forms.ImageField(required=False)
@@ -10,4 +10,18 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', 'bio', 'profile_picture', 'location']
+
+
+
+
+class BookReviewForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description', 'cover_image', 'published_date', 'category']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 5:
+            raise forms.ValidationError("The title must be at least 5 characters long.")
+        return title
 
