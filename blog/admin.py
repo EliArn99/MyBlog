9bbd2d40eefca3256c74b54d8b76a73f5a0bc6f9
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2025-01-21 13:04:45 +0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CustomUser, Category, Post, Comment, Tag, PostTag, ContactMessage, PrivacyPolicy, TermsOfService, PostLike, UserFollow
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(PostTag)
+admin.site.register(ContactMessage)
+admin.site.register(PrivacyPolicy)
+admin.site.register(TermsOfService)
+admin.site.register(PostLike)
+admin.site.register(UserFollow)
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
