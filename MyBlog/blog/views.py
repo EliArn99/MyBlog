3d3638d665f7ee5a9f408566d6c9c015e6c96fe5
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-27 15:15:35 +0200

    Update views.py

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'blog/home.html', {'user': request.user})
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Specify the backend when logging in
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')  # Or the appropriate redirect
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
