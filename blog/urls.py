167a852e5974e5a80553c2eed68bd7ce4cabd598
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-28 11:02:19 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('profile/', views.profile_view, name='profile'),  # Profile page URL
     path('profile/edit/', views.profile_edit_view, name='profile_edit'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/create/', views.post_create, name='post_create'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:post_id>/like/', views.like_post, name='like_post'),
+    path('post/<int:post_id>/dislike/', views.dislike_post, name='dislike_post'),
     path('accounts/', include('allauth.urls')),  # Allauth URL маршрути
     path('register/', views.register_view, name='register'),
 # urls.py
