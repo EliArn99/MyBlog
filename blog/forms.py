cf6f6c40e6a10483be15b2942dabf7c82f0189ac
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-28 10:15:17 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,27 +1,31 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
-from .models import Book
-class CustomUserCreationForm(UserCreationForm):
-    bio = forms.CharField(widget=forms.Textarea, required=False)
-    profile_picture = forms.ImageField(required=False)
-    location = forms.CharField(max_length=100, required=False)
 
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'bio', 'profile_picture', 'location']
+        fields = [
+            'username',
+            'email',
+            'role',
+            'password1',
+            'password2'
+        ]
 
 
 
 
-class BookReviewForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = Book
-        fields = ['title', 'author', 'description', 'cover_image', 'published_date', 'category']
+        model = CustomUser
+        fields = ['bio', 'profile_picture']  # Fields we want to allow users to update
 
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-        if len(title) < 5:
-            raise forms.ValidationError("The title must be at least 5 characters long.")
-        return title
 
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'bio', 'profile_picture']
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows': 3}),
+        }
