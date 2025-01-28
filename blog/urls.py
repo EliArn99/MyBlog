d4d4263972bdaca36f86240056912bde9d9387db
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-28 10:15:33 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
-
+    path('profile/', views.profile_view, name='profile'),  # Profile page URL
+    path('profile/edit/', views.profile_edit_view, name='profile_edit'),
     path('accounts/', include('allauth.urls')),  # Allauth URL маршрути
     path('register/', views.register_view, name='register'),
 # urls.py
