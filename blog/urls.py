07bee5d713b08390dcf802d0d587c212cac8e7c6
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-29 10:28:10 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 # urls.py
 from django.urls import path, include
+from django.views.i18n import set_language
+
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
@@ -11,6 +13,7 @@ urlpatterns = [
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:post_id>/like/', views.like_post, name='like_post'),
     path('post/<int:post_id>/dislike/', views.dislike_post, name='dislike_post'),
+    path('set-language/', set_language, name='set_language'),
     path('accounts/', include('allauth.urls')),  # Allauth URL маршрути
     path('register/', views.register_view, name='register'),
 # urls.py
