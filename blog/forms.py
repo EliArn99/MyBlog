853957b43fe51b54dd13181e5cb54e9b0c0a3dae
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-01-31 10:45:40 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, Post, Category, Comment
+from django.contrib.auth.models import User
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -44,10 +46,15 @@ class CategoryForm(forms.ModelForm):
         fields = ['name', 'slug']
 
 
-
-
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
